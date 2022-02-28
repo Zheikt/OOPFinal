45b97c6e6d3fe30c8f66d151de3d91e20e275ec5
Samuel Howsden <79162770+Zheikt@users.noreply.github.com>
2022-02-28 16:26:47 -0700

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,10 @@
+package edu.neumont.oop;
+
+import edu.neumont.oop.Model.BattleshipGameBoard;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println(new BattleshipGameBoard());
+    }
+}
