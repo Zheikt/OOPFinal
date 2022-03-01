0c86c49d7ff20fdfb700d8079bafce35ae5fb6c0
zsteber <100639026+zsteber@users.noreply.github.com>
2022-03-01 11:43:11 -0700

    Piece position updated, added base move / jump method

--- Controller/CheckersGamePlay.java
+++ Controller/CheckersGamePlay.java
@@ -1,34 +1,61 @@
 package edu.neumont.oop.Controller;
 
+import edu.neumont.oop.Model.CheckersBoard;
+import edu.neumont.oop.Model.CheckersPieces;
+
+import java.util.ArrayList;
 
 public class CheckersGamePlay {
-    int x1, y1, x2, y2;
+
+    int x1, y1, x2, y2, currentX, currentY;
+    boolean previousMove;
     boolean isJump;
 
-    public void movePiece(int x1, int y1, int x2, int y2, boolean isJump) {
 
-        this.x1 = x1;
-        this.y1 = y1;
-        this.x2 = x2;
-        this.y2 = y2;
-        this.isJump = isJump;
+   public int[] getStartingPosition() {
+
+        int[] position = new int[2];
+        position[0] = x1;
+        position[1] = y1;
+        return position;
     }
 
-    public int[] getStartPosition() {
+    public int[] getEndingPosition() {
 
-        int[] sPosition = new int[2];
-        sPosition[0] = x1;
-        sPosition[1] = y1;
-        return sPosition;
+        int[] position = new int[2];
+        position[0] = x2;
+        position[1] = y2;
+        return position;
     }
 
-    public int[] getEndPosition() {
+    public int[] getCurrentPosition(){
 
-        int[] ePosition = new int[2];
-        ePosition[0] = x2;
-        ePosition[1] = y2;
-        return ePosition;
+        int[] currentPosition = new int[2];
+        currentPosition[0] = currentX;
+        currentPosition[1] = currentY;
+        return currentPosition;
     }
 
+    public void jump() {
+        int[] moveStartingPos;
+        int[] moveEndingPos;
+    }
 
+   public CheckersPieces[] getJumpedPieces(CheckersBoard board) {
+
+        if (isJump) {
+
+            ArrayList<CheckersPieces> pieces = new ArrayList<>();
+
+            int pieceX = (x1 + x2) / 2;
+            int pieceY = (y1 + y2) / 2;
+
+            if (!previousMove) {
+            }
+
+            pieces.trimToSize();
+            return pieces.toArray(new CheckersPieces[1]);
+        } else
+            return null;
+    }
 }
