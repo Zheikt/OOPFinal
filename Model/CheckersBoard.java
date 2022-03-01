d791149abc6bea59c0a0b6a18a4a291bacb86b33
Samuel Howsden <sjdhowsden@gmail.com>
2022-03-01 12:15:04 -0700

    Changed the board to be an 8x8 and modified CheckersBoard.boardString() to match

--- Model/CheckersBoard.java
+++ Model/CheckersBoard.java
@@ -3,22 +3,21 @@ package edu.neumont.oop.Model;
 public class CheckersBoard extends GameBoard{
 
     public CheckersBoard(){
-        super(8,4);
+        super(8,8);
     }
 
     @Override
     public String boardString() {
         StringBuilder builder = new StringBuilder();
 
+        String[] backgroundColors  = new String [] {"\u001B[48;5;196m", "\u001B[48;5;0m"};
+        String color;
+
         for (int row = 0; row < super.getBoard().length; row++) {
-            for (Token token : super.getBoard()[row]) {
-                if(row % 2 == 0){
-                    builder.append("\u001B[48;5;196m   ");
-                }
-                builder.append("\u001B[48;5;0m   ").append(token == null ? "" : (token.getColor() + "0\u001B[0m\u001B[48;5;0m"));
-                if(row % 2 != 0){
-                    builder.append("\u001B[48;5;196m   ");
-                }
+            for (int token = 0; token < super.getBoard()[row].length; token++) {
+                color = backgroundColors[(token + row) % 2];
+
+                builder.append(color).append(super.getBoard()[row][token] == null ? "   " : (super.getBoard()[row][token].getColor() + " 0 \u001B[0m"));
             }
             builder.append("\u001B[0m\n");
         }
