c5e3ee9a251b2f1545d6099e39d8265a79227256
LorenzoF765 <100639060+LorenzoF765@users.noreply.github.com>
2022-03-07 08:50:40 -0700

    Menu is roughly drafted / complete

--- Main.java
+++ Main.java
@@ -1,6 +1,7 @@
 package edu.neumont.oop;
 
 import edu.neumont.oop.Controller.BattleshipGameplay;
+import edu.neumont.oop.Controller.MainMenu;
 import edu.neumont.oop.Model.BattleshipGameBoard;
 import edu.neumont.oop.Model.BattleshipPlayer;
 import edu.neumont.oop.Model.CheckersBoard;
@@ -10,27 +11,30 @@ public class Main {
     public static void main(String[] args) {
         //System.out.println(new BattleshipGameBoard());
         //System.out.println(new CheckersBoard());
-    //Test Code for ship Placement
-        BattleshipPlayer testPlayer = new BattleshipPlayer("Tester");
-
-        testPlayer.setShip(0, 0, 0, 1, 5);
-        testPlayer.setShip(1, 1, 0, 1, 4);
-        testPlayer.setShip(2, 2, 0, 1, 3);
-        testPlayer.setShip(3, 3, 0, 1, 3);
-        testPlayer.setShip(4, 4, 0, 1, 2);
-
-        System.out.println(testPlayer.getShipBoard());
-
-        BattleshipPlayer testPlayer2 = new BattleshipPlayer("Test Subject 2");
-
-        testPlayer2.setShip(8, 2, 0, 1, 5);
-        testPlayer2.setShip(1, 1, 1, 0, 4);
-        testPlayer2.setShip(2, 2, 0, 1, 3);
-        testPlayer2.setShip(9, 9, 0, -1, 3);
-        testPlayer2.setShip(4, 4, -1, 0, 2);
-
-        System.out.println(testPlayer2.getShipBoard());
-
-        new BattleshipGameplay(new BattleshipPlayer[]{testPlayer, testPlayer2}).gamePlayLoop();
+        //Test Code for ship Placement
+        MainMenu mMenu = new MainMenu();
+        mMenu.introduction();
+//        BattleshipPlayer testPlayer = new BattleshipPlayer("Tester");
+//
+//        testPlayer.setShip(0, 0, 0, 1, 5);
+//        testPlayer.setShip(1, 1, 0, 1, 4);
+//        testPlayer.setShip(2, 2, 0, 1, 3);
+//        testPlayer.setShip(3, 3, 0, 1, 3);
+//        testPlayer.setShip(4, 4, 0, 1, 2);
+//
+//        System.out.println(testPlayer.getShipBoard());
+//
+//        BattleshipPlayer testPlayer2 = new BattleshipPlayer("Test Subject 2");
+//
+//        testPlayer2.setShip(8, 2, 0, 1, 5);
+//        testPlayer2.setShip(1, 1, 1, 0, 4);
+//        testPlayer2.setShip(2, 2, 0, 1, 3);
+//        testPlayer2.setShip(9, 9, 0, -1, 3);
+//        testPlayer2.setShip(4, 4, -1, 0, 2);
+//
+//        System.out.println(testPlayer2.getShipBoard());
+//
+//        new BattleshipGameplay(new BattleshipPlayer[]{testPlayer, testPlayer2}).gamePlayLoop();
+//    }
     }
 }
