41a6d6ceb93705a9e0dbe8e9e65fc9b2a50475dd
Samuel Howsden <sjdhowsden@gmail.com>
2022-03-08 20:54:42 -0700

    Updated test code

--- Main.java
+++ Main.java
@@ -1,6 +1,8 @@
 package edu.neumont.oop;
 
+import edu.neumont.oop.Controller.BattleshipGameplay;
 import edu.neumont.oop.Controller.MainMenu;
+import edu.neumont.oop.Model.BattleshipPlayer;
 
 public class Main {
 
@@ -10,27 +12,26 @@ public class Main {
         //Test Code for ship Placement
         MainMenu mMenu = new MainMenu();
         mMenu.introduction();
-//        BattleshipPlayer testPlayer = new BattleshipPlayer("Tester");
+//        BattleshipPlayer testPlayer = new BattleshipPlayer("Tester", false);
 //
-//        testPlayer.setShip(0, 0, 0, 1, 5);
-//        testPlayer.setShip(1, 1, 0, 1, 4);
-//        testPlayer.setShip(2, 2, 0, 1, 3);
-//        testPlayer.setShip(3, 3, 0, 1, 3);
-//        testPlayer.setShip(4, 4, 0, 1, 2);
+//        testPlayer.setShip(0, 0, 0, 1, 5, "Carrier");
+//        testPlayer.setShip(1, 1, 0, 1, 4, "Battleship");
+//        testPlayer.setShip(2, 2, 0, 1, 3, "Cruiser");
+//        testPlayer.setShip(3, 3, 0, 1, 3, "Submarine");
+//        testPlayer.setShip(4, 4, 0, 1, 2, "Destroyer");
 //
 //        System.out.println(testPlayer.getShipBoard());
 //
-//        BattleshipPlayer testPlayer2 = new BattleshipPlayer("Test Subject 2");
+//        BattleshipPlayer testPlayer2 = new BattleshipPlayer("Test Subject 2", false);
 //
-//        testPlayer2.setShip(8, 2, 0, 1, 5);
-//        testPlayer2.setShip(1, 1, 1, 0, 4);
-//        testPlayer2.setShip(2, 2, 0, 1, 3);
-//        testPlayer2.setShip(9, 9, 0, -1, 3);
-//        testPlayer2.setShip(4, 4, -1, 0, 2);
+//        testPlayer2.setShip(8, 2, 0, 1, 5, "Carrier");
+//        testPlayer2.setShip(1, 1, 1, 0, 4, "Battleship");
+//        testPlayer2.setShip(2, 2, 0, 1, 3, "Cruiser");
+//        testPlayer2.setShip(9, 9, 0, -1, 3, "Submarine");
+//        testPlayer2.setShip(4, 4, -1, 0, 2, "Destroyer");
 //
 //        System.out.println(testPlayer2.getShipBoard());
 //
 //        new BattleshipGameplay(new BattleshipPlayer[]{testPlayer, testPlayer2}).gamePlayLoop();
-//    }
     }
 }
